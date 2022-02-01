7d40c716b2c52c0308ee2153297f1d82bff7bac7
pawooten@gmail.com <pawooten@gmail.com>
2022-01-31 20:24:17 -0500

    chore: cleanup

--- app.js
+++ app.js
@@ -6,11 +6,6 @@ const staticDirectories = [ 'audio', 'data', 'images', 'js', 'wwwroot'];
 staticDirectories.forEach(directory => {
   app.use(express.static(directory));
 });
-// app.use(express.static('audio'));
-// app.use(express.static('data'));
-// app.use(express.static('images'));
-// app.use(express.static('js'));
-// app.use(express.static('wwwroot'));
 
 const port = 3000;
 app.listen(port, () => {
