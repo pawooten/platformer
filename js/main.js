afe7300d30df633ee9c4d05dae0a9b00e3143117
pawooten@gmail.com <pawooten@gmail.com>
2022-02-21 20:01:54 -0500

    feat: coin count w/vhud

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ PlayState.preload = function () {
     this.game.load.image('grass:1x1', 'grass_1x1.png');
     this.game.load.image('hero', 'hero_stopped.png');
     this.game.load.image('invisible-wall', 'invisible_wall.png');
+    this.game.load.image('icon:coin', 'coin_icon.png');
+    this.game.load.image('font:numbers', 'numbers.png');
 
     this.game.load.audio('sfx:jump', 'jump.wav');
     this.game.load.audio('sfx:coin', 'coin.wav');
@@ -20,6 +22,7 @@ PlayState.preload = function () {
     this.game.load.spritesheet('spider', 'spider.png', 22, 22);
 };
 PlayState.init = function () {
+    this.coinPickupCount = 0;
     this.game.renderer.renderSession.roundPixels = true;
     this.keys = this.game.input.keyboard.addKeys({
         left: Phaser.KeyCode.LEFT,
@@ -43,13 +46,28 @@ PlayState.create = function () {
     
     this.game.add.image(0, 0, 'background');
     this._loadLevel(this.game.cache.getJSON('level:1'));
+
+    this._createHud();
+};
+PlayState._createHud = function () {
+    const NUMBERS_STR = '0123456789X ';
+    this.coinFont = this.game.add.retroFont('font:numbers', 20, 26, NUMBERS_STR, 6);
+    let coinIcon = this.game.make.image(0, 0, 'icon:coin');
+    let coinScoreImg = this.game.make.image(coinIcon.x + coinIcon.width, coinIcon.height / 2, this.coinFont);
+
+    this.hud = this.game.add.group();
+    this.hud.add(coinIcon);
+    this.hud.position.set(10, 10);
+    this.hud.add(coinScoreImg);
 };
 PlayState.update = function () {
+    this.coinFont.text = `x${this.coinPickupCount}`;
     this._handleCollisions();
     this._handleInput();
 };
 PlayState._onHeroVsCoin = function(hero, coin) {
     this.sfx.coin.play();
+    this.coinPickupCount++;
     coin.kill();
 }
 PlayState._onHeroVsEnemy = function(hero, enemy) {
